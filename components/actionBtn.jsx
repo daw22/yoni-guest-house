489cb0aae0a1c7bf4e73e6865186c35300fe843e
dawit <dawitxxd@gmail.com>
2024-11-23 14:24:30 +0300

    action btn date params fix

--- components/actionBtn.jsx
+++ components/actionBtn.jsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 import { addDate } from '@/utils/date';
 
 function ActionBtn() {
-  const today = addDate(0);
-  const tomorrow = addDate(1);
+  const today = addDate(new Date(), 0);
+  const tomorrow = addDate(new Date(), 1);
   const url = `/booking?checkin=${today}&checkout=${tomorrow}&type=single`;
   return (
     <Link href={url}>
